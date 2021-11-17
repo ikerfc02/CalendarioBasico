76eaf8c9d669961bfb3cf5740b529a4bf173fc95
ikerfc <ikerfc02@gmail.com>
2021-11-17 13:13:11 +0100

    03 Añadido avanzar fecha

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -14,4 +14,40 @@ public class CalendarioBasico {
         mes = 1;
         año = 1;
     }
+    
+    /**
+     * Devuelve la fecha actual
+     */
+    public String obtenerFecha(){
+        return (dia + "-" + mes + "-" + año);
+    }
+    
+    /**
+     * Modifica la fecha a la descrita
+     */
+    public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAño){
+        dia = nuevoDia;
+        mes = nuevoMes;
+        año = nuevoAño;
+    }
+    
+    /**
+     * Avanza un día en la fecha
+     */
+    public void avanzarFecha(){
+        if (dia <= 30){
+            dia = dia + 1;
+        }
+        if (dia == 31){
+            dia = 1;
+            mes = mes +1;
+        }
+        if (mes == 13){
+            año = año +1;
+            mes = 1;
+        }
+        if (año == 100){
+            año = 1;
+        }
+    }
 }
